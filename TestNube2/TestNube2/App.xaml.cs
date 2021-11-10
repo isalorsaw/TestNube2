012b072ebe779ac3d53bfcf41bf2d295efd1f8cb
isalorsaw <isalorsaw@hotmail.com>
2021-11-09 20:09:21 -0600

    Agregar archivos de proyecto.

---
+++ TestNube2/TestNube2/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestNube2
+{
+    public partial class App : Application
+    {
+        public static string url;
+        public App()
+        {
+            //url = "http://192.168.1.10/webservice2/";
+            url = "https://uthprograweb2.000webhostapp.com/webservice/";
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TestNube2/TestNube2/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestNube2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            if(verifBD())
+            {
+                //llenarUsers();
+            }
+            else
+            {
+                cerrar();
+            }
+        }
+        public async void cerrar()
+        {
+            await DisplayAlert("Mensaje", "Problemas de Conectividad. La aplicacion se va a cerrar", "OK");
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+        private bool verifBD()
+        {
+            try
+            { 
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+
+                byte[] response = cliente.UploadValues(App.url + "conexion2.php", "POST", parametros);
+                string c = Encoding.ASCII.GetString(response);
+                //DisplayAlert("Mensaje", c, "OK");
+                if (c.Equals("1")) return true;
+                else return false;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
